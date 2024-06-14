7b8ba87f9b66c8f7cd14b577ba4836328677d220
bhavishya2601 <bgarg2050bhav@gmail.com>
2024-06-14 20:12:16 +0530

    hosting problem correction

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ let songs
 let currFolder
 async function getSongs(folder) {
   currFolder = folder
-  let a = await fetch(`http://127.0.0.1:3000/${folder}/`)
+  let a = await fetch(`/${folder}/`)
   let response = await a.text()
   let div = document.createElement('div')
   div.innerHTML = response
@@ -71,7 +71,7 @@ const playMusic = (music, pause = false) => {
 
 // Display albums on the page
 async function displayAlbums(){
-  let a = await fetch(`http://127.0.0.1:3000/songs/`)
+  let a = await fetch(`/songs/`)
   let response = await a.text()
   let div = document.createElement('div')
   div.innerHTML = response
@@ -83,7 +83,7 @@ async function displayAlbums(){
       
     if (e.href.includes('/songs')){
       let folder = e.href.split('/').slice(-2)[0]
-      let a = await fetch(`http://127.0.0.1:3000/songs/${folder}/info.json`)
+      let a = await fetch(`/songs/${folder}/info.json`)
       let response = await a.json()
       cardContainer.innerHTML += `
       <div data-folder="${folder}" class="card">
